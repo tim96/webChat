aded3915698a9b1ca8fad72aa08136aa0aadd3eb
Alexander <tim96@yandex.ru>
2015-12-24 21:18:51 +0400

    Add 'ChatHandler' for keep part of application logic.

---
+++ src/AppBundle/Handler/ChatHandler.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tim
+ * Date: 12/24/2015
+ * Time: 9:14 PM
+ */
+
+namespace TimConfigBundle\Handler\Base;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class ChatHandler
+{
+    /** @var  ContainerInterface */
+    protected $container;
+    /** @var  EntityManager */
+    protected $om;
+
+    public function __construct(ContainerInterface $container, ObjectManager $om)
+    {
+        $this->container = $container;
+        $this->om = $om;
+    }
+
+    protected function getManager()
+    {
+        return $this->om;
+    }
+
+    protected function getContainer()
+    {
+        return $this->container;
+    }
+}
