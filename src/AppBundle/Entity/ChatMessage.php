1c48ef151a50b91a21543609ca5b9a80307159ba
Alexander <tim96@yandex.ru>
2015-12-24 21:02:22 +0400

    Add new fields to 'ChatMessage';

--- src/AppBundle/Entity/ChatMessage.php
+++ src/AppBundle/Entity/ChatMessage.php
@@ -21,6 +21,26 @@ class ChatMessage
      */
     private $id;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="text", type="string", length=255)
+     */
+    public $text;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=100)
+     */
+    public $username;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    public $createdAt;
 
     /**
      * Get id
@@ -31,5 +51,76 @@ class ChatMessage
     {
         return $this->id;
     }
-}
 
+    /**
+     * Set text
+     *
+     * @param string $text
+     *
+     * @return ChatMessage
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Get text
+     *
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     *
+     * @return ChatMessage
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return ChatMessage
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+}
